62cdbfd97c614afd7839ea4418818fb0de857e5b
aya mohamed <96661510+Aya-M-Hammad@users.noreply.github.com>
2022-01-02 13:58:03 +0200

    Create scratch-story.php

---
+++ scratch-story.php
@@ -0,0 +1,57 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+
+
+<style type="text/css" media="all">
+   html { 
+   direction:rtl;
+
+          background: url(background.jpg) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          }
+         
+          body {
+               
+              background: none;
+               
+          }
+           
+          .container {
+               
+              text-align: center;
+              margin-top: 100px;
+              width: 450px;
+               
+          }
+           
+          h1 {
+    font-size: 2.5rem;
+    font-family: cairo;
+}
+</style>
+  <title>Aya Hsoub Scratsh Story </title>
+</head>
+<body>
+    <div class="container">
+      
+          <h1>Aya Hsoub Scratsh Story</h1>
+          
+          <br>
+          
+      
+          <div id="video"></div>
+          <iframe src="https://scratch.mit.edu/projects/622634155/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>    <!-- jQuery first, then Bootstrap JS. -->
+      </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
+</html>
